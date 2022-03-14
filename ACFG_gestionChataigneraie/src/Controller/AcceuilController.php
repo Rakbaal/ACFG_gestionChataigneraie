60e858532be3f11c668245ac6654989c180e0548
theoGarin <theo.garin@campus-la-chataigneraie.org>
2022-03-14 09:56:42 +0100

    Ajouts Template et controller Acceuil

---
+++ ACFG_gestionChataigneraie/src/Controller/AcceuilController.php
@@ -0,0 +1,20 @@
+<?php
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+class MonController extends AbstractController
+{
+    
+    /**
+     * @Route("/acceuil", name="Page d'acceuil")
+     */
+    public function Acceuil()
+    {
+        return new Response("Bienvenue Login");
+    }
+}
+
+?>
